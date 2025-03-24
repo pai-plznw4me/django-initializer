54b12e34ef00deac8a72e7b047e6d2786f9aa084
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 <plznw4me@publicai.co.kr>
2025-03-24 10:36:00 +0900

    card app 추가
    bankbook app 추가
    
    bankhistory app 추가
    
     purchaseapp 추가

--- projectbuckets/erp/card/models.py
+++ projectbuckets/erp/card/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
-from bank.models import Bank
+from bankbook.models import Bankbook
 from department.models import Department
 from employee.models import Employee
 
 
 class Card(models.Model):
 	name = models.CharField(max_length=100)
-	desacription = models.TextField()
+	desacription = models.TextField(null=True, blank=True)
 	code = models.CharField(max_length=20)
-	bank = models.ForeignKey(Bank, on_delete=models.SET_NULL, null=True, blank=True)
+	bank = models.ForeignKey(Bankbook, on_delete=models.SET_NULL, null=True, blank=True)
 	owner = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
 	department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
